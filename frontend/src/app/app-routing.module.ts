ce35459f9c7f7ebc12412b99e339d89f4281f2a6
Rodolfo-Muller <ro_muller111@hotmail.com>
2021-04-23 02:01:12 -0300

    Nuevas Rutas

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -3,7 +3,7 @@ import { Routes, RouterModule } from "@angular/router";
 
 
 const routes: Routes = [
-  { path: "", redirectTo: "/auth/login", pathMatch: "full" },
+  { path: "", redirectTo: "/", pathMatch: "full" },
   { path: "auth", loadChildren: "./components/auth/auth.module#AuthModule" },
   {path: "order", loadChildren: "./components/order/order.module#OrderModule"}
 ];
